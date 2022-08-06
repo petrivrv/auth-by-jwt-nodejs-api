a7507677ef12ed608d7bb3ab671310c35d4f328e
vscode <vs@gmail.com>
2022-08-06 20:41:49 +0300

    impl login and getUser

--- apiController.js
+++ apiController.js
@@ -1,23 +1,32 @@
 const {PrismaClient}= require('@prisma/client')
+const bcrypt = require('bcryptjs')
+const {validationResult} = require('express-validator')
+const jwt = require('jsonwebtoken')
 const prisma = new PrismaClient()
 const {JwtUser} = prisma
+
+const genToken = (id,name)=>{
+    const payload ={id,name}
+    return jwt.sign(payload, process.env.SECRET, {expiresIn:"12h"})
+}
 
 class ApiController{
     async reg(req,res){
         const {name, pswd, role} = req.body
         try{
-            const newUser = await JwtUser.findUnique({
+            const errors = validationResult(req)
+            if(!errors.isEmpty()) return res.status(400).json({msg: errors})
+            const findUser = await JwtUser.findUnique({
                 where: {
                   name //это соответств name: name
                 }
             })
-            if(newUser) return res.status(400).json({msg: 'Пользователь уже существует'})
-            const user = await JwtUser.create({
-                data: { name, pswd, role }
+            if(findUser) return res.status(400).json({msg: 'Пользователь уже существует'})
+            const hashPswd = bcrypt.hashSync(pswd, 8);
+            const createUser = await JwtUser.create({
+                data: { name, pswd: hashPswd, role }
             })
-            res.json(user)
-
-
+            return res.json({msg: 'Пользователь успешно создан'})
         }
         catch(e){
             console.log(e)
@@ -27,7 +36,16 @@ class ApiController{
     }
 
     async login(req,res){
+        const {name, pswd, role} = req.body
         try{
+            const dbUser = await JwtUser.findUnique({ where: {name} })
+            if(!dbUser) return res.status(400).json({msg: 'Пользователь не найден'})
+            const validPswd = bcrypt.compareSync(pswd, dbUser.pswd)
+            if(!validPswd) return res.status(400).json({msg: 'Введен неверный пароль'})
+            else {
+                const token = genToken(dbUser.id, dbUser.name)
+                res.json({token})
+            }
 
         }
         catch{
@@ -38,11 +56,12 @@ class ApiController{
 
     async getUsers(req,res){
         try{
-            res.json( 'я тут')
+            const users = await JwtUser.findMany({})
+            res.json(users)
 
         }
         catch{
-            
+            console.log(e)
         }
     }
 }
--- apiRouter.js
+++ apiRouter.js
@@ -1,21 +1,17 @@
 const router = require('express').Router()
 const controller = require('./apiController')
+const {check} = require('express-validator')
 
 router.get('/', async (req, res, next) => {
   res.send({ message: 'Ok api is working' });
 });
 
-router.post('/reg', controller.reg)
+router.post('/reg',[
+    check('name','Имя не может быть пустым').notEmpty(),
+    check('pswd','Пароль должен быть больше 3 символов').isLength({min:3, max:200})
+], controller.reg)
 router.post('/login', controller.login)
 router.get('/users', controller.getUsers)
 
-/*
-router.get('/prod', async (req, res, next) => {
-  const prod = await prisma.prod.findMany({})
-  res.json(prod)
-});
-
-
-*/
 
 module.exports = router;
